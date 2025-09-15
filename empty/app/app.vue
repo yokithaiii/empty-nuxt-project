f1eae719ad6e8e4d7660a1c0fd04af04f05ad29a
spxrtxk <spartakvasilev1999@gmail.com>
2025-09-15 21:52:06 +0900

    fix: awdaw

--- empty/app/app.vue
+++ empty/app/app.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>TEST 2 WITH PACKAGES "dependencies": {
+    "@nuxt/eslint": "^0.7.4",
+    "@nuxt/image": "^1.8.1",
+    "@nuxt/ui": "^3.3.3",
+    "eslint": "^9.17.0",
+    "nuxt": "^4.1.1",
+    "vue": "^3.5.21",
+    "vue-router": "^4.5.1",
+    "vue-tg": "^0.8.0"
+  }</h1>
+    <UButton>Button</UButton>
+    <UApp>
+      <NuxtPage />
+    </UApp>
   </div>
 </template>
--- empty/nuxt.config.ts
+++ empty/nuxt.config.ts
@@ -1,5 +1,35 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import Tailwind from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+		rootTag: 'main',
+		head: {
+			title: 'Bodyline - marathons app',
+			script: [
+				{
+					src: 'https://telegram.org/js/telegram-web-app.js',
+				},
+			],
+		},
+		rootAttrs: {
+			class: 'bg-[var(--ui-bg)]',
+		},
+	},
+ modules: [
+		'@nuxt/ui', 
+		'@nuxt/eslint', 
+		'@nuxt/image',
+	],
+  css: ['~/assets/css/main.css'],
+  vite: {
+		plugins: [Tailwind()],
+	},
+  ui: {
+		theme: {
+			colors: ['primary', 'secondary', 'tertiary', 'info', 'success', 'warning', 'error'],
+		},
+	},
 })
